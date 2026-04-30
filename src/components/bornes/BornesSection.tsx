53882c0d95c484b20bfbeb07ebe39b51790e8f31
msallak <hilyas707@gmail.com>
2026-04-30 15:38:19 +0200

    Envoi sur la branche master

--- src/components/bornes/BornesSection.tsx
+++ src/components/bornes/BornesSection.tsx
@@ -123,7 +123,7 @@ export default function BornesSection({ dict }: Props) {
                 style={{ borderRadius: "calc(2rem - 0.5rem)" }}
               >
                 <Image
-                  src="/electric-car-power-charging.jpg"
+                  src="/electric-car-power-charging.jfif"
                   alt="Connecteur de borne de recharge installé par TF Technics"
                   fill
                   className="object-cover object-center"
